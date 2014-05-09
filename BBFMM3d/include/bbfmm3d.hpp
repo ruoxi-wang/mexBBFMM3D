433370046e38e0a529778013a85bc5c5eaf1df57
Ruoxi Wang <ruoxi@stanford.edu>
2014-05-09 13:55:50 -0700

    add BBFMM3D

---
+++ BBFMM3d/include/bbfmm3d.hpp
@@ -0,0 +1,20 @@
+
+/*!	\file bbfmm3d.hpp
+  Defines different types of kernel
+*/
+#ifndef __bbfmm3d_hpp__
+#define __bbfmm3d_hpp__
+
+#include "bbfmm.h"
+#include "compute.hpp"
+#include "mex.h"
+#include "matrix.h"
+#include "environment.hpp"
+#include "test.hpp"
+#include <Eigen/Core>
+#include "kernelfun.hpp"
+#include "kernel_Types.hpp"
+
+
+
+#endif //(__bbfmm3d_hpp__)
