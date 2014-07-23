047b4dbcc0490ea7272104582193ad0a0bc7aba2
Ruoxi Wang <ruoxi@stanford.edu>
2014-07-23 14:30:18 -0700

    add delete command to example

--- example.m
+++ example.m
@@ -1,6 +1,8 @@
 %% Compile mex code for new kernel
 clear all
-syms r;                          % distance of two points (radius)
+delete('./*.o');
+delete('./BBFMM3D/output/*.bin'); % make sure to delete output file if kernel is changed
+syms r;                           % distance of two points (radius)
 kernel = 1 ./ r.^2;               % radius basis function 
 % kernel = exp(-r^2);
 outputfile = 'mexFMM3D';
@@ -16,22 +18,23 @@ make(r,kernel,homogen,symmetry,outputfile);
 Ns  = 10000;    % Number of sources in simulation cell
 Nf  = 10000;    % Number of fields in simulation cell
 m   = 2;        % number of columns of H
-
+L = 1.0;        % Length of simulation cell (assumed to be a cube)
 % 3-D locations, stored column-wise i.e. (x | y | z)
-source = rand(Ns,3) - 0.5;
-% field = rand(Nf,3) - 0.5; 
+
+source = (rand(Ns,3) - 0.5) .* L;
+% field = (rand(Nf,3) - 0.5) .* L; 
 field = source;
 
 H = rand(Ns,m); 
 
 nCheb = 4;          % Number of Chebyshev nodes per dimension
-L = 1.0;            % Length of simulation cell (assumed to be a cube)
 level = 3;          % Level of FMM tree
 use_chebyshev = 1;  % 1: chebyshev interpolation; 0: uniform interpolation
 
 
 %%
 % Compute matrix-vectors product QH
+% QH = mexFMM3D(source, field,H,nCheb, level, L, use_chebyshev);
 [QH,QH_exact] = mexFMM3D(source, field,H,nCheb, level, L, use_chebyshev);
 
 
